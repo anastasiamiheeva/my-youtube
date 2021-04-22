554abeb569fb93f3dabd80ad05a081be3a857b8f
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-04-22 14:16:51 +0300

    day 2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const gloAcademyList = document.querySelector('.glo-academy-list');
+const trendingList = document.querySelector('.trending-list');
+const musicList = document.querySelector('.music-list');
 
 const createCard = (dataVideo) => {
 
   const imgUrl = dataVideo.snippet.thumbnails.high.url;
-  const videoId = dataVideo.id.videoId;
+  const videoId = typeof dataVideo.id === 'string' ? dataVideo.id : dataVideo.id.videoId;
   const titleVideo = dataVideo.snippet.title;
+  // const viewCount = dataVideo.statistics ? dataVideo.statistics.viewCount : null;
+  const viewCount = dataVideo.statistics?.viewCount;
   const dateVideo = dataVideo.snippet.publishedAt;
   const channelTitle = dataVideo.snippet.channelTitle;
 
@@ -18,8 +22,9 @@ const createCard = (dataVideo) => {
   </div>
   <h3 class="video-title">${titleVideo}</h3>
   <div class="video-info">
+    ${viewCount ? `<span class="video-views">${viewCount} views</span>` : ''}
     <span class="video-counter">
-      <span class="video-date">${dateVideo}</span>
+      <span class="video-date">${(new Date(dateVideo)).toLocaleString('ru-RU')}</span>
     </span>
     <span class="video-channel">${channelTitle}</span>
   </div>
@@ -35,6 +40,77 @@ const createList = (wrapper, listVideo) => {
   })
 };
 
+createList(gloAcademyList, gloAcademy);
+createList(trendingList, trending)
+createList(musicList, music)
 
+// youtube API
 
-createList(gloAcademyList, gloAcademy)
+const authBtn = document.querySelector('.auth-btn');
+const userAvatar = document.querySelector('.user-avatar');
+
+const handleSuccessAuth = data => {
+  console.log('data: ', data);
+  authBtn.classList.add('hide');
+  userAvatar.classList.remove('hide');
+  userAvatar.src = data.getImageUrl();
+  userAvatar.alt = data.getName();
+
+  getChannel()
+}
+
+const handleNoAuth = () => {
+  authBtn.classList.remove('hide');
+  userAvatar.classList.add('hide');
+  userAvatar.src = '';
+  userAvatar.alt = '';
+}
+
+const handleAuth = () => {
+  gapi.auth2.getAuthInstance().signIn()
+  
+}
+
+const handleSignout = () => {
+  gapi.auth2.getAuthInstance().signOut()
+}
+
+const updateStatusAuth = data => {
+  data.isSignedIn.listen(() => {
+    updateStatusAuth(data);
+  });
+
+  if(data.isSignedIn.get()) {
+    const userData = data.currentUser.get().getBasicProfile();
+    handleSuccessAuth(userData)
+  } else {
+    handleNoAuth();
+  }
+}
+
+function initClient() {
+  gapi.client.init({
+      'clientId': CLIENT_ID,
+      'scope': 'https://www.googleapis.com/auth/youtube.readonly',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  }).then(() => {
+    updateStatusAuth(gapi.auth2.getAuthInstance());
+    authBtn.addEventListener('click', handleAuth);
+    userAvatar.addEventListener('click', handleSignout)
+  }).catch(() => {
+    authBtn.removeEventListener('click', handleAuth);
+    userAvatar.removeEventListener('click', handleSignout)
+  })
+}
+
+gapi.load('client:auth2', initClient);
+
+getChannel = () => {
+  gapi.client.youtube.channels.list({
+    part: 'snippet, statistics',
+    id: 'UCVswRUcKC-M35RzgPRv8qUg',
+
+  }).execute((response) => {
+    console.log('response: ', response);
+  })
+}
